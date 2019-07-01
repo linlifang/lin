4a3f6e08735cd36bd541ba4ec0f19a760e4a9e72
空心酒伴我 <@DESKTOP-2H58BBR>
2019-07-01 20:19:16 +0800

    接收微信公众号平台发送给我们的消息

--- src/main/java/org/llf/weixin/MessageReceiverController.java
+++ src/main/java/org/llf/weixin/MessageReceiverController.java
@@ -1,4 +1,8 @@
 package org.llf.weixin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,16 +10,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/kemao_2/weixin/receiver")
 public final class MessageReceiverController {
+	private static final Logger LOG=LoggerFactory.getLogger(MessageReceiverController.class);
 	public String echo(//
-	@RequestParam("signature") String signature,//
+	@RequestParam("signature") String sigature,//
 	@RequestParam("timestamp") String timestamp,//
 	@RequestParam("nonce") String nonce,//
 	@RequestParam("echostr") String echostr//
 		//
-		) {
-		return echostr;
-		
+		) {return echostr;
+	
 	}
+	@PostMapping
+	public String onMessage(@RequestBody String xml) {
+		LOG.trace("收到的消息原文:\n{}\n..................", xml);
+		return "succcess";
+	}
+	
 	
 
 }
